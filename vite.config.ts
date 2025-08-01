0ae0af11ee99d76ee2e8e6aa583aa3e7380d284a
Torben Haack <t128n@ipv4.8shield.net>
2025-08-01 16:33:55 +0200

    fix: remove username from vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/t128n/packy/",
+	base: "/packy/",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
