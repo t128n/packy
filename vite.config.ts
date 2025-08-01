b42e5a6fcdf568af016a2d0f3099176688125271
Torben Haack <t128n@ipv4.8shield.net>
2025-08-01 16:28:24 +0200

    feat: prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/t128n/packy/",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
